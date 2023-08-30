77817d0d8dcf752c24affde57ed191f480486961
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-08-30 09:19:11 +0300

    .

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,49 +12,37 @@ function About() {
         </div>
         <div className="col-md-6">
           <p className="lead">
-            Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır
-            metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat
-            numune kitabı oluşturmak üzere bir yazı galerisini alarak
-            karıştırdığı 1500'lerden beri endüstri standardı sahte metinler
-            olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle
-            kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de
-            sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset
-            yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi
-            Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile
-            popüler olmuştur.
+          The world's tallest mountain ranges form when pieces of Earth's crust—called plates—smash against each other in a process called plate tectonics, and buckle up like the hood of a car in a head-on collision. The Himalaya in Asia formed from one such massive wreck that started about 55 million years ago. Thirty of the world’s highest mountains are in the Himalaya. The summit of Mount Everest, at 29,035 feet (8,850 meters), is the highest point on Earth.
+
+The tallest mountain measured from top to bottom is Mauna Kea, an inactive volcano on the island of Hawaii in the Pacific Ocean. Measured from the base, Mauna Kea stands 33,474 feet (10,203 meters) tall, though it only rises 13,796 feet (4,205 meters) above the sea.
+
+Volcanic mountains form when molten rock from deep inside the Earth erupts through the crust and piles up on itself. The islands of Hawaii were formed by undersea volcanoes, and the islands seen above water today are the remaining volcano tops. Well-known volcanoes on land include Mount St. Helens in Washington State and Mount Fuji in Japan. Sometimes volcanic eruptions break down mountains instead of building them up, like the 1980 eruption that blew the top off Mount St. Helens.
+
+When magma pushes the crust up but hardens before erupting onto the surface, it forms so-called dome mountains. Wind and rain pummel the domes, sculpting peaks and valleys. Examples include the Black Hills of South Dakota and the Adirondack Mountains of New York. Plateau mountains are similar to dome mountains, but form as colliding tectonic plates push up the land without folding or faulting. They are then shaped by weathering and erosion.
+
+Other types of mountains form when stresses within and between the tectonic plates lead to cracking and faulting of the Earth's surface, which forces blocks of rock up and down. Examples of fault-block mountains include the Sierra Nevada in California and Nevada, the Tetons in Wyoming, and the Harz Mountains in Germany.
+
+Impact of Mountains on Habitats and Geopolitics
+Mountains often serve as geographic features that define natural borders of countries. Their height can influence weather patterns, stalling storms that roll off the oceans and squeezing water from the clouds. The other side is often much drier. The rugged landscapes even provide refuge—and protection—for fleeing and invading armies.
           </p>
           <p className="lead">
-            Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır
-            metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat
-            numune kitabı oluşturmak üzere bir yazı galerisini alarak
-            karıştırdığı 1500'lerden beri endüstri standardı sahte metinler
-            olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle
-            kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de
-            sıçramıştır.
+          The word forest broadly describes an area that has a large number of trees. There are three general types of forest that exist: temperate, tropical, and boreal. Experts estimate that these forests cover approximately one-third of Earth’s surface.
+
+Temperate forests are found across eastern North America and Eurasia. The temperatures of temperate forests vary throughout the year because of the four distinct seasons at these latitudes. Precipitation is abundant and lends to fertile soil that is able to support diverse flora like maples, oak, and birch. Deer, squirrels, and bears are just a few examples of the fauna that call temperate forests home.
+
+Tropical forests are common to areas near the equator, such as Southeast Asia, sub-Saharan Africa, and Central America. Temperatures in tropical forests have been reported to range between 20 and 31°C (68 and 88°F). Tropical rainforests are the epitome of biodiversity.
           </p>
         </div>
       </div>
       <div className="row my-4">
         <div className="col-md-6">
           <p className="lead">
-            Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır
-            metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat
-            numune kitabı oluşturmak üzere bir yazı galerisini alarak
-            karıştırdığı 1500'lerden beri endüstri standardı sahte metinler
-            olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle
-            kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de
-            sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset
-            yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi
-            Lorem.
+          During a sunset, mountain peaks lose sunlight later than areas around them. From a viewer observing a mountain, a shadow can appear ascending from the base to the peak. And conversely, during a sunrise, mountain peaks get sunlight before the areas around them and a shadow can appear descending from the peak to the base.
+
+This phenomenon occurs because the Earth is a sphere. As the altitude increases, the hours of daylight become longer. It makes the sun rise earlier and set later.
           </p>
           <p className="lead">
-            Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır
-            metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat
-            numune kitabı oluşturmak üzere bir yazı galerisini alarak
-            karıştırdığı 1500'lerden beri endüstri standardı sahte metinler
-            olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle
-            kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de
-            sıçramıştır.
+          The third type of forest is the boreal forest, also known as taiga. Boreal forests, one of the world’s largest land biomes, are found across Siberia, Scandinavia, and North America (Alaska and Canada). Boreal forests have a significant role in removing carbon dioxide from the atmosphere. Temperatures in boreal forests are, on average, below freezing. Conifers, spruce, fir, and pine trees are the predominant needle-leaf plant species in boreal forests. Moose and deer are just a couple of examples of large herbivorous mammals in this environment. Most birds native to the taiga migrate to find warmer conditions during the forest’s harsh winters.
           </p>
         </div>
         <div className="col-md-6">
