b2b3980e420fc61f15438b2c9f25930aeabebfc2
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-08-30 13:48:30 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Container from "./components/Container";
+import {Routes} from 'react-router-dom';
 import { ThemeProvider } from "./components/Context/ThemeContext";
 
 function App() {
