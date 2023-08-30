528b086c6050175cde25b67295fbd18671a7bbb0
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-08-30 14:56:05 +0300

    .

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,8 +1,29 @@
 import React from "react"
 
-function homepaga(){
+function Homepage(){
     return (
+<div className="containet">
+<div className="row my-3">
+    <h5 className="display-4">Homepage</h5>
+    <div className="row">
+        <div className="col">
+        <img src={require("https://images.unsplash.com/photo-1643316037362-e81fc45e6357?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=6")}/>
+            <div className="w-100" alt ="home"></div>
+                <div classname="col">
+                    <p className="card-text">
+                    A mountain pass is a navigable route through a mountain range or over a ridge. Since mountain ranges can present formidable barriers to travel, passes have played a key role in trade, war, and both human and animal migration throughout history. At lower elevations it may be called a hill pass.
+                    Roads and railways have long been built through passes. Some high and rugged passes may have tunnels bored underneath a nearby mountainside, as with the Eisenhower Tunnel bypassing Loveland Pass in the Rockies, to allow faster traffic flow throughout the year.
+The top of a pass is frequently the only flat ground in the area, and is a high vantage point. In some cases this makes it a preferred site for buildings. If a national border follows a mountain range, a pass over the mountains is typically on the border, and there may be a border control or customs station, and possibly a military post. For instance, Argentina and Chile share the world's third-longest international border, 5,300 kilometres (3,300 mi) long, which runs north–south along the Andes mountains including 42 mountain passes.
+On a road over a pass, it is customary to have a small roadside sign giving the name of the pass and its elevation above mean sea level.
+Apart from offering relatively easy travel between valleys, passes also provide a route between two mountain tops with a minimum of descent. As a result, it is common for tracks to meet at a pass; this often makes them convenient routes even when travelling between a summit and the valley floor. Passes traditionally were places for trade routes, communications, cultural exchange, military expeditions etc. A typical example is the Brenner pass in the Alps.
+</p>
 
-    )
-}
-#bootstrap
+<p className="lead">
+Some mountain passes above the tree line have problems with snow drift in the winter. This might be alleviated by building the road a few meters above the ground, which will make snow blow off the road.
+    </p> </div>
+
+                </div> 
+                </div>
+        </div>
+    </div>
+    )}
