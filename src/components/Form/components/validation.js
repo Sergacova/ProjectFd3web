342ca37a7a3b07834aef61ba07cfd34fc48a1864
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-08-30 15:34:22 +0300

    .

--- src/components/Form/components/validation.js
+++ src/components/Form/components/validation.js
@@ -2,13 +2,12 @@ import * as yup from "yup";
 
 const contactSchema = yup.object().shape({
   fullname: yup.string("Enter a Valid String").required("Required"),
-  email: yup
-    .string("Enter a Valid String")
+  email: yup.string("Enter a Valid Input")
     .email("Enter a Valid Email")
     .required("Required"),
-  message: yup.string("Enter a Valid String").required("Required"),
-  phone: yup.string("Enter a Valid String"),
-  website: yup.string("Enter a Valid String"),
+  message: yup.string("Enter a Valid Input").required("Required"),
+  phone: yup.string("Enter a Valid Input"),
+  website: yup.string("Enter a Valid Input"),
 });
 
 export default contactSchema;
